872d2208b186a2424066700a6b5980d81f5bf716
rahul744 <60570811+rahul744@users.noreply.github.com>
2020-08-27 13:14:20 +0530

    Add files via upload

--- javaproject.java
+++ javaproject.java
@@ -1,25 +1,16 @@
 package demo;
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
 public class javaproject{
-	
-	  public static void main(String args[]) 
-	     {
-	     List<String> fileNames = new ArrayList<>();
-	        try {
-	              DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get("C:\\Users\\chnna\\Desktop\\New folder"));
-	                 for (Path path : directoryStream)
-	                 {
-	                   fileNames.add(path.toString());
-	                 }
+
+	  public static void main(String args[]) {
+		  String[] path;
+		  int count=0;
+	        File f = new File("C:\\Users\\chnna\\Desktop\\New folder");
+	        path = f.list();
+	        for (String p : path) {
+	                      count++;
+	        	      System.out.println(p);
 	            }
-	        catch (IOException ex) {
-	                               }
-	             System.out.println("File Count:"+fileNames.size());
+	            System.out.println("---Count of files is :"+ count);
 	     }
 	}
